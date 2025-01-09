7cfd7d1a7059c7e8b6896111d86e236176d86e17
dawntrann6880 <khoanguyen251318@gmail.com>
2025-01-09 22:37:05 +0700

    Header
    - Những thứ như thanh tìm kiếm, logo,... thì làm trong header hết, những màn hình khác chỉ cần include header vào thôi là được

--- visualmain.php
+++ visualmain.php
@@ -123,21 +123,6 @@
     </style>
 </head>
 <body>
-    <!-- Header -->
-    <header>
-        <div class="logo">
-            <img src="images/logo.png" alt="Oops Logo">
-        </div>
-        <div class="search-bar">
-            <input type="text" placeholder="Tìm kiếm sản phẩm...">
-            <span>🔍</span>
-        </div>
-        <div class="user-actions">
-            <span>🔔</span>
-            <span>👤</span>
-            <span>🛒</span>
-        </div>
-    </header>
 
     <!-- Banner -->
     <div class="banner">
