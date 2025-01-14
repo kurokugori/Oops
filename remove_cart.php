72be3e0e495ecb195bced09e2ce4b3391f863a73
dawntrann6880 <khoanguyen251318@gmail.com>
2025-01-14 22:21:37 +0700

    Add lại các file đã chỉnh sửa của cường
    - thiếu file config nên t thêm config vào và chỉnh lại các đường dẫn

--- remove_cart.php
+++ remove_cart.php
@@ -1,14 +1,17 @@
 <?php
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $product_id = $_POST['product_id'];
+// Kiểm tra nếu có ID sản phẩm được gửi từ URL
+if (isset($_GET['id'])) {
+    $productId = $_GET['id'];
 
-    if (isset($_SESSION['cart'][$product_id])) {
-        unset($_SESSION['cart'][$product_id]);
+    // Kiểm tra nếu sản phẩm tồn tại trong giỏ hàng (session)
+    if (isset($_SESSION['cart'][$productId])) {
+        unset($_SESSION['cart'][$productId]); // Xóa sản phẩm khỏi giỏ hàng
     }
-}
 
-header("Location: cart.php");
-exit();
+    // Sau khi xóa, chuyển hướng lại trang giỏ hàng (cart.php)
+    header('Location: cart.php');
+    exit;
+}
 ?>
